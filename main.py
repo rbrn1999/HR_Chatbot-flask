890cd672f93a3076af1ab7bddaa7d45917c3cbf8
Audi <audi@sparkamplify.com>
2021-11-28 20:46:35 +0800

    feat: basic html for each route

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ def start_work():
 @app.route("/end_work", methods=['GET'])
 def end_work():
     return render_template('endWork.html')
+
+@app.route("/leave_permission", methods=['GET'])
+def leave_permission():
+    return render_template('leavePermission.html')
+  
+@app.route("/attendance", methods=['GET'])
+def attendance():
+    return render_template('attendance.html')
+
+@app.route("/personal_information", methods=['GET'])
+def personal_information():
+    return render_template('personalInformation.html')
+  
+@app.route("/company_information", methods=['GET'])
+def company_information():
+    return render_template('companyInformation.html')
+
+@app.route("/report", methods=['GET'])
+def report():
+    return render_template('report.html')
